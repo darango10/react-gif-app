6a85dbc21c899ab053ddd3cb6e7df5bf34a99a5e
Daniel Arango <you@example.comarangomonsalve.daniel@gmail.com>
2020-07-17 20:26:53 -0500

    docs

---
+++ docs/precache-manifest.8f69101d203b18f4ae701e5430fff075.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9f394b758ed66f7268b6df413452aa8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01551a3405185355a47b",
+    "url": "/static/css/main.3b73bca9.chunk.css"
+  },
+  {
+    "revision": "7f3198e8ee718484dab7",
+    "url": "/static/js/2.8ecfbc0f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8ecfbc0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01551a3405185355a47b",
+    "url": "/static/js/main.2d973b8c.chunk.js"
+  },
+  {
+    "revision": "a70e4ae6bdd791779146",
+    "url": "/static/js/runtime-main.1e57df78.js"
+  }
+]);
